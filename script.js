65b4dd224de9b9c34bdd977d64bee90193fe3435
TezzT <76174431+TezzT@users.noreply.github.com>
2023-12-22 23:26:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    let currentSectionIndex = 0;
+
+    // Detect when the user scrolls
+    window.addEventListener('scroll', function() {
+        // Find the current section based on the scroll position
+        const scrollPosition = window.scrollY;
+        const currentSection = Array.from(sections).findIndex(section => {
+            const rect = section.getBoundingClientRect();
+            return rect.top <= 0 && rect.bottom > 0;
+        });
+
+        if (currentSection !== -1 && currentSection !== currentSectionIndex) {
+            currentSectionIndex = currentSection;
+            console.log(`Current Section: ${currentSectionIndex + 1}`);
+        }
+    });
+
+    // Scroll to the next or previous section based on the scroll direction
+    window.addEventListener('wheel', function(event) {
+        const scrollDirection = event.deltaY > 0 ? 'down' : 'up';
+
+        if (scrollDirection === 'down' && currentSectionIndex < sections.length - 1) {
+            currentSectionIndex++;
+        } else if (scrollDirection === 'up' && currentSectionIndex > 0) {
+            currentSectionIndex--;
+        }
+
+        sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 document.querySelector('.topButt').addEventListener('click', function() {
     const aboutSection = document.querySelector('.home');
     aboutSection.scrollIntoView({ behavior: 'smooth' });
