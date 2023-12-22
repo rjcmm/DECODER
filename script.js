3756805bf169cca3005b33d5d02a4ddbd02bee59
TezzT <76174431+TezzT@users.noreply.github.com>
2023-12-23 00:44:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.section');
     let currentSectionIndex = 0;
 
+// Detect when the user scrolls
     window.addEventListener('scroll', function() {
+// Find the current section based on the scroll position
         const scrollPosition = window.scrollY;
         const currentSection = Array.from(sections).findIndex(section => {
             const rect = section.getBoundingClientRect();
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+// Scroll to the next or previous section based on the scroll direction
     window.addEventListener('wheel', function(event) {
         const scrollDirection = event.deltaY > 0 ? 'down' : 'up';
 
@@ -27,6 +30,60 @@ document.addEventListener('DOMContentLoaded', function() {
 
         sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
     });
+
+
+
+/*----------------------------------------------------------------  */
+
+
+
+let startY = 0;
+let lastScrollTime = 0;
+const scrollCooldown = 500; // Set the cooldown period in milliseconds
+
+// Detect when the user starts touching the screen
+window.addEventListener('touchstart', function(event) {
+    startY = event.touches[0].clientY;
+});
+
+// Detect when the user moves their finger
+window.addEventListener('touchmove', function(event) {
+    // Calculate the vertical distance moved
+    const deltaY = event.touches[0].clientY - startY;
+
+    // Set a threshold to determine if it's a swipe
+    const swipeThreshold = 130;
+
+    if (Math.abs(deltaY) > swipeThreshold) {
+        const currentTime = new Date().getTime();
+
+        // Check if enough time has passed since the last scroll
+        if (currentTime - lastScrollTime > scrollCooldown) {
+            const scrollDirection = deltaY > 0 ? 'up' : 'down'; // Reverse the conditions
+
+            if (scrollDirection === 'down' && currentSectionIndex < sections.length - 1) {
+                currentSectionIndex++;
+            } else if (scrollDirection === 'up' && currentSectionIndex > 0) {
+                currentSectionIndex--;
+            }
+
+            sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
+
+            // Update the last scroll time
+            lastScrollTime = currentTime;
+
+            // Reset startY to prevent continuous scrolling
+            startY = event.touches[0].clientY;
+        }
+    }
+});
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    let currentSectionIndex = 0;
+
 });
 
 document.querySelector('.topButt').addEventListener('click', function() {
